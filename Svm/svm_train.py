cfa24b759f717094dddcbe62af475d2feae2c38b
wayaya <dongsibei2002@163.com>
2019-11-20 12:06:59 +0800

    添加必要的注释

--- Svm/svm_train.py
+++ Svm/svm_train.py
@@ -16,28 +16,31 @@ def load_data_libsvm(data_file):
     label = []
     f = open(data_file)
     for line in f.readlines():
-        lines = line.strip().split(' ')
+        cols = line.strip().split(' ')  # 分成多个列单元
         
         # 提取得出label
-        label.append(float(lines[0]))
+        label.append(float(cols[0]))  # 第一列是真实值
         # 提取出特征，并将其放入到矩阵中
-        index = 0
-        tmp = []
+        index = 0  # 数据特征索引 索引号从0开始
+        tmp = []  # 每行的数据，每行样本数据
         for i in range(1, len(lines)):
-            li = lines[i].strip().split(":")
-            if int(li[0]) - 1 == index:
+            items = cols[i].strip().split(":")  # 每列被分成两项
+            # 由于特征索引是从0开始的，所以要减去1
+            if int(items[0]) - 1 == index:
                 tmp.append(float(li[1]))
             else:
-                while(int(li[0]) - 1 > index):
-                    tmp.append(0)
+                # 如果当前索引号没有对应的特征值，则添加0值；前提是存储的数据已经按照索引号排序
+                while(int(items[0]) - 1 > index):
+                    tmp.append(0)  # 添加0值进行占位
                     index += 1
-                tmp.append(float(li[1]))
+                tmp.append(float(items[1]))
             index += 1
+        # 如果特征值长度不够，则补充0值保证数据长度一致
         while len(tmp) < 13:
             tmp.append(0)
-        data.append(tmp)
+        data.append(tmp)  # 将完整的数据加入到data变量
     f.close()
-    return np.mat(data), np.mat(label).T
+    return np.mat(data), np.mat(label).T  # 根据列表创建矩阵
 
 if __name__ == "__main__":
     # 1、导入训练数据
